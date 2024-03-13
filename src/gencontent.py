b7419b4ceff3fb3f8524bd55a331c292319fd3a1
robby-o <robby.olson11@gmail.com>
2024-03-12 22:47:44 -0700

    site geneartor

---
+++ src/gencontent.py
@@ -0,0 +1,47 @@
+import os
+from pathlib import Path
+
+from markdown_blocks import markdown_to_html_node
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(
+            dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {
+          dest_path} using {template_path}")
+
+    with open(from_path, "r") as file:
+        markdown_content = file.read()
+    with open(template_path, "r") as file:
+        template = file.read()
+
+    html = markdown_to_html_node(markdown_content).to_html()
+    title = extract_title(markdown_content)
+
+    template = template.replace(
+        "{{ Title }}", title).replace("{{ Content }}", html)
+
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+
+    with open(dest_path, "w") as file:
+        file.write(template)
+
+
+def extract_title(markdown):
+    lines = markdown.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    raise ValueError("No title found")
